5f7b754daf9f077c8193f9e2c898ffa638b6c008
Abhijat Bharadwaj <96429535+Keymii@users.noreply.github.com>
2023-05-02 20:54:12 +0530

    uProcessor enitity created

--- 1bitreg.vhd
+++ 1bitreg.vhd
@@ -13,19 +13,19 @@ end r_1bit;
 
 architecture bhv of r_4bit is
 begin
-P:process(wr, clock, D)
-begin
-if rising_edge(clock) then
-  if wr='1' then
-    output <= D;
-  else
-    null;
-  end if;
-  if reset='1' then
-	 output<='0';
-  end if;
-else
-  null;
-end if;
-end process;
+	P:process(wr, clock, D)
+	begin
+		if rising_edge(clock) then
+		  if wr='1' then
+			 output <= D;
+		  else
+			 null;
+		  end if;
+		  if reset='1' then
+			 output<='0';
+		  end if;
+		else
+		  null;
+		end if;
+	end process;
 end bhv;
