7d628a51deefed320b5d5f6845bfa9946eb7197e
KeeengPin <animeshkasba25@gmail.com>
2023-05-03 12:29:39 +0530

    sc_rr

--- subCircuit_RR.vhd
+++ subCircuit_RR.vhd
@@ -12,13 +12,20 @@ entity subCircuit_RR is
 			
 			rf_a1,rf_a2:out std_logic_vector(2 downto 0);
 			rf_d1,rf_d2:in std_logic_vector(15 downto 0);
-		 
+		   data_reg1, dat_reg2 :out std_logic_vector(15 downto 0)
     );
 	 
 end subCircuit_RR;
 architecture a3 of subCircuit_RR is
 
 begin
+ if (reg_read_1 = 1) then
+   rf_a1 <= instr(11 downto 9);
+	data_reg1 <= rf_d1;
+	
+ elsif (reg_read_2 = 1) then
+   rf_a2 <= instr(8 downto 6); 
+	data_reg2 <= rf_d2;
 
 end a3;
  
