42cee3848c013a9fb6c92d59a599ef0539e4504d
Shounak <21d070068@iitn.ac.in>
2023-05-06 18:17:55 +0530

    Hazards Updated

--- Hazard ADD NAND ADI.vhd
+++ Hazard ADD NAND ADI.vhd
@@ -3,7 +3,7 @@ library ieee;
 use std.standard.all;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all; 
-
+--Execution Stage
 entity hazard1 is
     port (
         instr_in:in std_logic_vector(15 downto 0);
@@ -16,7 +16,7 @@ architecture bhv of hazard1 is
 begin
 	process(instr_in,c,z)
 	begin
-		if (instr_in(15 down to 12)="0001" 
+		if (instr_in(15 down to 11)="0001" 
 			and (instr_in(5 down to 0)="000000" --5 down to 3 is R0(000),hence hazard
 				or (instr_in(5 down to 0)="000001" and z='1') 
 				or (instr_in(5 down to 0)="000010" and c='1') 
@@ -27,7 +27,7 @@ begin
 				or (instr_in(5 down to 0)="000111" and (c and z)='1')) 				
 				) then -- ADD _,_,R0
 			haz1<='1';
-		elsif (instr_in(15 down to 12)="0010" 
+		elsif (instr_in(15 down to 11)="0010" 
 			and (instr_in(5 down to 0)="000000" --5 down to 3 is R0(000),hence hazard
 				or (instr_in(5 down to 0)="000001" and z='1') 
 				or (instr_in(5 down to 0)="000010" and c='1') 
@@ -38,7 +38,7 @@ begin
 				or (instr_in(5 down to 0)="000111" and (c and z)='1')) 				
 				) then -- NAND _,_,R0
 			haz1<='1';
-		elsif(instr_in(15 down to 12)="0000" and instr_in(8 down to 6)="000") --8 down to 6 is R0(000),hence hazard
+		elsif(instr_in(15 down to 11)="0000" and instr_in(8 down to 6)="000") --8 down to 6 is R0(000),hence hazard
 			then -- ADI _,R0,Imm
 			haz1<='1';
 
